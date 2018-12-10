593d9f0a9e0f0d85ef25752d7ed43694b3acd3da
Mitch Belmer <mob3@rice.edu>
2018-12-09 19:27:23 -0600

    Git Request

--- code/src/App.js
+++ code/src/App.js
@@ -8,25 +8,29 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      username: ''
+      image: ''
     }
     this.handle_click = this.handle_click.bind(this)
   }
 
   handle_click() {
     //console.log("Success!")
-    axios.get('https://api.github.com/users/thehadadinator')
-      .then(response => this.setState({username: response.data.login}))
+    axios.get('http://localhost:3001/test?one="1"&two="1"&three="2"')
+      .then(response => this.setState({image: response.data}))
   }
 
   render() {
+    var src = this.state.image
     return (
       <div className="App">
         <header className="App-header">
         </header>
         <button className='button' onClick={this.handle_click}>Re-district</button>
-        <p>{this.state.username}</p>
+        <div>
+          <img src={src}/>
+        </div>
       </div>
+
     );
   }
 }
