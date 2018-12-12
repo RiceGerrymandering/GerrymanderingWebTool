33767df36b1a7ac321ea940e3ca9d322ecc70dd7
Mitch Belmer <mob3@rice.edu>
2018-12-11 22:28:00 -0600

    Testing simple heroku server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var fs = require('fs');
   
 // Creates a server which runs on port 3000 and  
 // can be accessed through localhost:3000 
-app.listen(process.env.PORT || 30001, function() { 
+app.listen(process.env.PORT || 3001, function() { 
     console.log('server running on port 3001'); 
 } ) 
   
@@ -18,40 +18,54 @@ app.get('/test', test);
 function test(req, res) { 
     console.log("Got Request!")
 
-      
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.send("test");
     // Use child_process.spawn method from  
     // child_process module and assign it 
     // to variable spawn 
-    var spawn = require("child_process").spawn; 
+    // var spawn = require("child_process").spawn; 
       
-    // Parameters passed in spawn - 
-    // 1. type_of_script 
-    // 2. list containing Path of the script 
-    //    and arguments for the script  
+    // // Parameters passed in spawn - 
+    // // 1. type_of_script 
+    // // 2. list containing Path of the script 
+    // //    and arguments for the script  
       
-    // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
-    // so, first name = Mike and last name = Will 
-    // var process = spawn('python',["test.py", 
+    // // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
+    // // so, first name = Mike and last name = Will 
+    // // var proc = spawn('python',["test.py", 
+    // //                         req.query.state,
+    // //                         req.query.competitiveness, 
+    // //                         req.query.compactness,
+    // //                         req.query.fairness] ); 
+
+    // var proc = spawn('python',["./rice/select_map.py", 
     //                         req.query.state,
     //                         req.query.competitiveness, 
     //                         req.query.compactness,
     //                         req.query.fairness] ); 
-
-     var process = spawn('python',["rice/select_map.py", 
-                            req.query.state,
-                            req.query.competitiveness, 
-                            req.query.compactness,
-                            req.query.fairness] ); 
+    // //console.log(proc)
+    // proc.stdout.pipe(proc.stdout);
   
-    // Takes stdout data from script which executed 
-    // with arguments and send this data to res object 
-    process.stdout.on('data', function(data) { 
-        console.log(data)
-        fs.readFile('rice/out.txt', 'utf8', function(err, contents) {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-            res.send(contents.toString());
-        });
-        //res.send(data.toString()); 
-    } ) 
+    // // Takes stdout data from script which executed 
+    // // with arguments and send this data to res object 
+    // proc.stdout.on('data', function(data) { 
+    //     console.log(data)
+    //     fs.readFile('rice/out.txt', 'utf8', function(err, contents) {
+    //         res.header('Access-Control-Allow-Origin', '*');
+    //         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    //         res.send(contents.toString());
+    //     });
+    //     //res.send(data.toString()); 
+    // } ) 
+
+    // proc.stdout.on('data', function(data) { 
+    //     console.log(data)
+    //     fs.readFile('out.txt', 'utf8', function(err, contents) {
+    //         res.header('Access-Control-Allow-Origin', '*');
+    //         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    //         res.send(contents.toString());
+    //     });
+    //     //res.send(data.toString()); 
+    // } ) 
 } 
