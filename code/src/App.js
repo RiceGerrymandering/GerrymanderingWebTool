328043d160aecb6cce772653aae67656c02c1051
andrew_hadad <aah6@rice.edu>
2018-12-12 09:39:24 -0600

    added stat support

--- code/src/App.js
+++ code/src/App.js
@@ -12,6 +12,89 @@ class App extends Component {
     this.handle_click = this.handle_click.bind(this)
   }
 
+    post_stats(map) {
+      var stats = document.getElementsByClassName("stats")[0];
+
+      var line_br = document.createElement("br");
+      line_br.innerHTML = "</br>"
+      stats.appendChild(line_br);
+
+      var competitiveness_score = map.c1;
+      var c1 = document.createElement("c1");
+      c1.innerHTML = "<code> Competitiveness Score: " + competitiveness_score + "</code>";
+      stats.appendChild(c1);
+
+      var compactness_score = map.c2;
+      var c2 = document.createElement("c2");
+      c2.innerHTML = "<code> Compactness Score: " + compactness_score + "</code>";
+      stats.appendChild(c2);
+
+      var fairness_score = map.f;
+      var f = document.createElement("f");
+      f.innerHTML = "<code> Fairness Score: " + fairness_score + "</code>";
+      stats.appendChild(f);
+
+      var line_br = document.createElement("br");
+      line_br.innerHTML = "</br>"
+      stats.appendChild(line_br);
+      var line_br = document.createElement("br");
+      line_br.innerHTML = "</br>"
+      stats.appendChild(line_br);
+
+      var competitiveness_percentile = map.c1p;
+      var c1p = document.createElement("c1p");
+      c1p.innerHTML = "<code> Competitiveness Percentile: " + competitiveness_percentile + "</code>";
+      stats.appendChild(c1p);
+
+      var compactness_percentile = map.c2p;
+      var c2p = document.createElement("c2p");
+      c2p.innerHTML = "<code> Compactness Percentile: " + compactness_percentile + "</code>";
+      stats.appendChild(c2p);
+
+      var fairness_percentile = map.fp;
+      var fp = document.createElement("fp");
+      fp.innerHTML = "<code> Fairness Percentile: " + fairness_percentile + "</code>";
+      stats.appendChild(fp);
+
+      var line_br = document.createElement("br");
+      line_br.innerHTML = "</br>"
+      stats.appendChild(line_br);
+      var line_br = document.createElement("br");
+      line_br.innerHTML = "</br>"
+      stats.appendChild(line_br);
+
+      var efficiency_gap = map.e;
+      var e = document.createElement("e");
+      e.innerHTML = "<code> Efficiency Gap: " + efficiency_gap + "</code>";
+      stats.appendChild(e);
+
+      var line_br = document.createElement("br");
+      line_br.innerHTML = "</br>"
+      stats.appendChild(line_br);
+      var line_br = document.createElement("br");
+      line_br.innerHTML = "</br>"
+      stats.appendChild(line_br);
+
+      var districts = document.createElement("districts");
+      districts.innerHTML = "<code> Districts: </code>";
+      stats.appendChild(districts);
+
+      var line_br = document.createElement("br");
+      line_br.innerHTML = "</br>"
+      stats.appendChild(line_br);
+
+      for (var key in map.d) {
+        if (map.d.hasOwnProperty(key)) {
+          var d_votes = map.d[key];
+          var r_votes = map.r[key];
+          var dist = document.createElement("dist");
+          dist.innerHTML = "<code> District " + key + " Democratic votes: " + d_votes + " Republican votes: " + r_votes + "</code> </br>";
+          stats.appendChild(dist);
+        }
+      }
+
+  }
+
   handle_click() {
     var competitiveness = parseInt(document.getElementById("competitiveness").value);
     var compactness = parseInt(document.getElementById("compactness").value);
@@ -20,9 +103,12 @@ class App extends Component {
     //var url = 'http://localhost:3001/test?competitiveness="' + competitiveness + '"&compactness="' + compactness + '"&fairness="' + fairness + "&state=" + state + ""
     //var url = 'https://test-gerry.herokuapp.com/test?competitiveness="' + competitiveness + '"&compactness="' + compactness + '"&fairness="' + fairness + "&state=" + state + ""
     var url = 'http://localhost:3001/test?competitiveness=' + competitiveness + '&compactness=' + compactness + '&fairness=' + fairness + '&state="' + state + '"'
+    //var url = 'http://localhost:3001/stats'
     console.log(url)
     axios.get(url)
       .then(response => this.setState({image: response.data}))
+    // axios.get(url)
+    //   .then(response => this.post_stats(response.data.map))
   }
 
   render() {
@@ -65,6 +151,9 @@ class App extends Component {
         <button className='button' onClick={this.handle_click}>Re-district</button>
         <div>
           <img src={src}/>
+        </div>
+        <div className="stats">
+          <div><code>District results after re-districting: </code></div>
         </div>
       </div>
 
